a7022c64b635794635f6e0ee2fc320c552691e5e
vs <vserzantovic81@gmail.com>
2021-03-03 00:24:43 +0200

    1st exercise - add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {ServerComponent} from './server/server.component';
 import {ServersComponent} from './servers/servers.component';
+import {WarningAlertComponent} from './warning-alert/warning-alert.component';
+import {SuccessAlertComponent} from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +23,5 @@ import {ServersComponent} from './servers/servers.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
